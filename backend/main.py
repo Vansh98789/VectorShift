3b23797b338a55e22dc27850c5b93d5899ebfca1
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2026-05-02 13:19:36 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vector-shift-omega.vercel.app/"],
+    allow_origins=["https://vector-shift-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
